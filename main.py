7b9ef965abc8c421f7a66487d4515fb21c157cf5
Joel Bello <joel8bl@gmail.com>
2023-06-28 20:01:41 -0400

    delete method added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import List
 from uuid import uuid4
 from fastapi import FastAPI
-from models import User, Gender, Role
+from models import User, Gender, Role, UUID
 
 app = FastAPI()
 
@@ -38,3 +38,11 @@ async def fetch_users():
 async def register_user(user: User):
     db.append(user)
     return {"id": user.id}
+
+@app.delete('/api/v1/users/{user_id}')
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return {"message": "User deleted successfully"}
+    return {"message": "User not found"}
